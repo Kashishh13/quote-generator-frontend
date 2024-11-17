9ca518a0edf9eadf66ddf47a5805d0ed051cce4c
kashish gupta <guptakashishh13@gmail.com>
2024-11-17 12:21:27 +0530

    first commit

--- Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Home.css'
-const Home = () => {
-  return (
-    <div className='bgi'>Home</div>
-  )
-}
-
-export default Home
---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import { Link } from 'react-router-dom'
+const Home = () => {
+  return (
+    <>
+    <div className='bgi'>Home</div>
+    
+    <h1 className='logo'>DICE LUCK
+   <Link to='/game'> <button className='play'>Lets's Play</button></Link>
+    </h1>
+    </>
+  )
+}
+
+export default Home
